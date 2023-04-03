1a38d74354c0ddaf7c07d233877672a76c8de08c
wangyes <wangyes1996@163.com>
2023-04-03 16:24:45 +0800

    init

--- packages/tools-fn/src/index.ts
+++ packages/tools-fn/src/index.ts
@@ -1,7 +1,7 @@
-import Fn  from "./Fn"
-import { defa_T } from "./types"
+// import Fn  from "./Fn"
+// import { defa_T } from "./types"
 
+export {default as Fn} from './Fn'
 
-
-const defa:defa_T={Fn}
-export default  defa
+// const defa:defa_T={Fn}
+// export default  defa
